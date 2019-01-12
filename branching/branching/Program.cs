2cfb11f00edd7a9d5e9c3706052df62462c6f805
AnsweR86 <41041085+AnsweR86@users.noreply.github.com>
2019-01-12 01:44:10 -0800

    Branching

--- branching/branching/Program.cs
+++ branching/branching/Program.cs
@@ -11,46 +11,49 @@ namespace branching
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
-            Console.ReadLine();
-
             Console.WriteLine("Please enter the package weight:");
-            int packageWeight = Convert.ToInt32(Console.ReadLine());
 
-            if (packageWeight > 50)
+            var packageWeight = Console.ReadLine();
+            int weight = Convert.ToInt32(packageWeight);
+
+            if (weight > 50)
             {
-                Console.WriteLine("Package too heavy to be shipped viaackage Express. Have a good day.");
+                Console.WriteLine("Package too heavy to be shipped via package Express. Have a good day.");
+                Console.ReadLine();
+                Environment.Exit(0);
             }
 
 
             Console.WriteLine("Please enter the package width:");
-            int packageWidth = Convert.ToInt32(Console.ReadLine());
+            string packageWidth = Console.ReadLine();
             Console.WriteLine("Please enter the package height:");
-            int packageHeight = Convert.ToInt32(Console.ReadLine());
+            string packageHeight = Console.ReadLine();
             Console.WriteLine("Please enter the package length:");
-            int packageLenght = Convert.ToInt32(Console.ReadLine());
+            string packageLenght = Console.ReadLine();
 
-            int total = packageWidth + packageHeight + packageLenght; 
-             if (total > 50)
+            float total = (Convert.ToInt32(packageWidth) + Convert.ToInt32(packageHeight) + Convert.ToInt32(packageLenght));
+            float amount = (total * weight / 100);
+            if (total > 50)
+
             {
                 Console.WriteLine("Package too big to be shipped via Package Express.");
 
             }
 
-            else  
+            else
             {
-                int result = total * packageWeight / 100;
-                Console.Write("Your estimated total for shipping this package is:  $"+result );
-                Decimal results = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Your estimated total for shipping this package is:  $" + amount.ToString("0.00") + Environment.NewLine+ "Thank You. Come again!");
+
             }
-                
-            
 
-
-            
-               
-            
-            
-            
+            Console.ReadLine();
         }
     }
 }
+
+
+        
+               
+            
+            
